b5347afacdbf0603e63d51fb333254ea73c01f70
Nathaniel Fenton <info@nathtech.dev>
2020-11-17 13:39:21 +0000

    fix(images): add plugin to transform absolute image paths to relative for gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,20 @@ module.exports = {
                 path: `${__dirname}/src/`,
             },
         },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `uploads`,
+                path: `${__dirname}/uploads`,
+            }
+        },
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
             resolve: 'gatsby-transformer-remark',
             options: {
                 plugins: [
-                    // 'gatsby-remark-autolink-headers',
+                    `gatsby-remark-normalize-paths`,
                     {
                         resolve: `gatsby-remark-images`,
                         options: {
